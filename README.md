c91869b832c88de469f787335518e5cad51222cd
Sam Trenholme <strenholme.usenet@gmail.com>
2018-08-01 10:28:05 -0700

    Initial version of Password generator

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# PassGen
-Simple secure password generation shell script
+# Generate secure passwords
+
+pwgen.sh allows the one to generate secure passwords for websites. 
+To use, do something like this:
+
+```
+echo 'Some secret string' > ~/.master_password # Only do this once
+pwgen.sh facebook.com
+```
+
+Replace “Some secret string” with a suitably hard to guess string, and 
+facebook.com with the site you need to log in to.
+
+To create another password for the site, edit the file `pwgen.sh`
+to give, say. facebook.com a higher `INDEX` value.
+
+## Dealing with website rules
+
+Obligatory: https://xkcd.com/936/
+
+The password “hlrehcv5kn4o545v” has (or had, until I posted this)
+a good deal more security than “Password1!” (the has 80 bits of
+entropy; the largest attacks I have seen, requiring entire datacenters
+filled with computers, can crack 64 bits; the second has at best 28 bits
+of security), but many sites consider the first password insecure and
+the second password secure.
+
+Making people have mixed case or punctuation in their password does
+not increase password security that much. However, it makes password
+management a pain.  For the majority of websites, adding the string
+`_Aa9` to the end of a password will allow a password to be considered
+“secure”; this password generator does that.
+
+While most sites accept the underscore `_` as a “punctuation”
+character, some do not.  Southwest, for example, does not even allow a `_`
+in a password, so pwgen.sh has a special “for Southwest, use : instead 
+of _ for punctuation” rule in it.
+
+To handle site specific rules, the script pwgen.sh will need to be
+edited by hand for each stire with a stupid password rule which 
+its default behavior does not cover.  There is a section giving
+examples of how to deal with site-specific rules.
+
+## Security considerations
+
+The master password needs to be kept secret.
+
+This tool should not be used on multi-user systems; untrusted users
+can peek at the master password while the password is being generated.
---
+++ pwgen.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+# Change this to something else to get a little bit more security
+# (but changing this will break passwords already being used)
+SUFFIX="Aa9"
+
+# All of the passwords are generated from the file .master_password
+# Edit that file to generate a secure master password.  Not that
+# the security of this system is dependent on the master password being
+# secret.
+SECRET=$( cat $HOME/.master_password )
+if [ -z "$SECRET" ] ; then
+	echo Could not find $HOME/.master_password
+	exit 1
+fi
+
+SITE="$1"
+if [ -z "$SITE" ] ; then
+	echo Usage: $0 {site} 
+	echo Where {site} is the website to generate a password for
+	exit 1
+fi
+
+# As a general rule, *most* sites require: 8-16 characters, at least one:
+# a) lower case letter
+# b) upper case letter
+# c) number
+# d) punctuation, where _ is considered punctuation
+# The tinyrg32 program adds _Aa1 to the end of each password which covers
+# *most* sites.  However, this being the internet, some sites have dumb
+# password rules we have to make exceptions for.  This script covers
+# most of them
+
+# Some sites will not allow a '_' character in their passwords.  That
+# in mind, we can optionally zap the '_' character.
+# Real world offender: timewarnercable.com
+ZAP='~'
+
+# Some sites do not consider _ punctuation.  
+# Make $CHANGE something besides '_' to change the '_' to another symbol
+# Real world offender: southwest.com
+CHANGE='_'
+
+# Some sites require passwords to be changed frequently.  This is actually
+# a reasonable security practice, and we support it
+INDEX=1
+
+# Some sites might require a password to be longer than 16 characters, or
+# to be shorter. The password length is 4 + (4 * LEN), so for a site which
+# can only handle passwords 12 characters long, make LEN 2; for a site which
+# can only handle 8 character passwords, make LEN 1.  For someone who needs
+# 24 chars in the password, make LEN 5
+LEN=3 # 16 character password
+
+##### SITE SPECIFIC RULES GO HERE #####
+if [ "$SITE" = "timewarnercable.com" ] ; then
+	ZAP='_' # Remove the _ at the end of the password
+fi
+if [ "$SITE" = "southwest.com" ] ; then
+	CHANGE=':' # Make the _ a : in the password
+fi
+if [ "$SITE" = "paypal.com" ] ; then
+	INDEX=2 # Increase index by 1 every time we change a password
+fi
+if [ "$SITE" = "idiot.example.com" ] ; then
+	LEN=2 # This makes the password shorter
+fi
+### END SITE SPECIFIC RULES ###
+
+# Make sure tinyrg32 is in the path; if not then compile and run it
+TINYRG32="tinyrg32"
+
+if ! command -v tinyrg32 > /dev/null 2>&1 ; then
+	cat > tinyrg32-$$.c << EOF
+#include <stdio.h> // cc -o tinyrg32 tinyrg32.c /////// Public domain code
+#include <stdint.h> // ./tinyrg32 --hex --numbers 'A tiny list of numbers'
+#define b(z) for(c=0;c<(z);c++) // ./tinyrg32 --binary-stream 'Es la niña'
+uint32_t c,e[40],f[40],g=19,h=13,r,s,t,n[40],i,k,y,z;void m(){int c,j=0;b(
+12)f[c+c%3*h]^=e[c+1];b(g){j=(c+j)&31;i=c*7%g;k=e[i++];k^=e[i%g]|~e[(i+1)%
+g];n[c]=k>>j|k<<(32-j);}for(c=39;c--;f[c+1]=f[c])e[c]=n[c]^n[(c+1)%g]^n[(c
++4)%g];*e^=1;b(3)e[c+h]^=f[c*h]=f[c*h+h];}int main(int p,char**v){char *q=
+v[--p],*x=0;for(;;m()){b(3){for(r=0;r<4;){f[c*h]^=k=(*q?*q&255:1)<<8*r++;e
+[16+c]^=k;if(!*q++){b(17)m();b(p<3?8:89*p){if(~t&1)m();s=e[(t&1)+1];r=(p&3
+)-2?c:1;b(4){i=s;if(p&4){x=v[p-2];y=z=z?z:*v[p-1]%16;i&=31;i+=i<8?50:89;}s
+>>=8;printf(p==2||p&4?"%c":"%02x",255&i);}if((++t%8==0||(p&22)==2)&&p-2&&!
+y){puts("");}c=r;if(y&&!--z)puts(*x==95?x:"");}if(x)puts(x);return 0;}}}}}
+EOF
+	cc -o tinyrg32-$$ tinyrg32-$$.c
+	TINYRG32=./tinyrg32-$$
+fi
+
+# If you need a really large index, change this command line
+$TINYRG32 --make --many _$SUFFIX $LEN "$LEN:$SECRET:$SITE" | head -$INDEX | \
+	tail -1 | tr -d "$ZAP" | tr '_' "$CHANGE"
+
+rm -f ./tinyrg32-$$ ./tinyrg32-$$.c ./tinyrg32-$$.exe
