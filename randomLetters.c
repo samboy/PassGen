cf933157133c3cd92234945a078503ee99f22e43
Sam Trenholme <strenholme.usenet@gmail.com>
2018-08-04 05:50:27 -0700

    randomLetters.c: Add license and documentation

--- randomLetters.c
+++ randomLetters.c
@@ -1,3 +1,20 @@
+/* Donated to the public domain 2018 by Sam Trenholme (CC0 "license")
+ * https://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * This small C program generates 32 random letters with 5 bits of 
+ * entropy each, giving us a string with 160 bits of total entropy.
+ *
+ * a is the file pointer to /dev/urandom
+ * b is the loop iterator; change "b<32" to give the output string another
+ *   length
+ * i is the character the users sees when running this program; we use a 
+ *   simple small algorithm to convert an 8-bit random number in to a 
+ *   5-bit alphanumeric random number
+ *
+ * The alphabet is non-standard: "23456789abcdefghijklmnopqrstuvwx" (where
+ * a binary 0 becomes "2", a binary 1 "3", a binary 8 "a", and so on)
+ */
+
 #include <stdio.h>
 
 int main() {
